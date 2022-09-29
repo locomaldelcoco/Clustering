6911f7287683e2cc0ea1b15911a5e0fcdf649462
Leandr0Fr <leandro00fr@gmail.com>
2022-09-29 19:38:15 -0300

    format

--- src/cluster/Vertice.java
+++ src/cluster/Vertice.java
@@ -4,15 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Vertice {
-	private long _x,_y;
+	private long _x, _y;
 	private Set<Integer> _vecinos;
-	
+
 	public Vertice(long x, long y) {
 		_x = x;
 		_y = y;
 		_vecinos = new HashSet<>();
 	}
-	
+
 	public void agregarVecino(int vecino) {
 		if (vecino < 0) {
 			throw new IllegalArgumentException("No puede existir vecinos negativos");
