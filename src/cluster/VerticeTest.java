709cb09b25f795d332ba739089f2bbd6101c02ee
Leandr0Fr <leandro00fr@gmail.com>
2022-09-29 19:42:29 -0300

    add Junit in patch and VerticeTest

---
+++ src/cluster/VerticeTest.java
@@ -0,0 +1,14 @@
+package cluster;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VerticeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
