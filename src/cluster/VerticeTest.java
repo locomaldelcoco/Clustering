25de6007a33d59827d4422fe9e3ae86ae9e32190
Leandr0Fr <leandro00fr@gmail.com>
2022-09-29 19:51:48 -0300

    add Tests VerticeTest

--- src/cluster/VerticeTest.java
+++ src/cluster/VerticeTest.java
@@ -1,14 +1,33 @@
 package cluster;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VerticeTest {
+	private Vertice vertice;
+
+	@Before
+	public void inicializarVertice() {
+		vertice = new Vertice(10, 10);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void agregarVecinoNegativoTest() {
+		vertice.agregarVecino(-1);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void agregarVecinoPositivoTest() {
+		vertice.agregarVecino(1);
+		assertTrue(vertice.get_vecinos().contains(1));
+	}
+
+	@Test
+	public void agregarVecinoCeroTest() {
+		vertice.agregarVecino(0);
+		assertTrue(vertice.get_vecinos().contains(0));
 	}
 
 }
