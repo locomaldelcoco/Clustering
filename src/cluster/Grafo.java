e0fce0aac0bba352c4e66b0566f5f5c026aadd84
Leandr0Fr <leandro00fr@gmail.com>
2022-09-29 21:01:30 -0300

    add class Grafo

---
+++ src/cluster/Grafo.java
@@ -0,0 +1,13 @@
+package cluster;
+
+import java.util.ArrayList;
+
+public class Grafo {
+	private ArrayList<Vertice> _vertices;
+	private ArrayList<Arcos> _arcos;
+	
+	public Grafo () {
+		_vertices = new ArrayList<>();
+		_arcos = new ArrayList<>();
+	}
+}
