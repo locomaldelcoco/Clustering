072eb57f4006f87eb4b100b8561ea445c4905cce
Gastón <gaelceart@gmail.com>
2022-10-01 16:15:07 -0300

    ADD class Arco

---
+++ src/cluster/Arco.java
@@ -0,0 +1,24 @@
+package cluster;
+
+public class Arco {
+	private Vertice _verticeA;
+	private Vertice _verticeB;
+	private long _distancia;
+
+	public Arco(Vertice verticeA, Vertice verticeB) {
+		_verticeA = verticeA;
+		_verticeB = verticeB;
+		_distancia = calcularDistancia();
+	}
+
+	private long calcularDistancia() {
+		return (long) Math.sqrt(
+				Math.pow(_verticeB.get_x() - _verticeA.get_x(),2) +
+				Math.pow(_verticeB.get_y() - _verticeA.get_y(),2)
+				);
+	}
+	
+	private long getDistancia() {
+		return _distancia;
+	}
+}
