25dab69d16d9c6ab108da0097b4f5f84eb6c327d
Gastón <gaelceart@gmail.com>
2022-10-01 17:07:28 -0300

    CHANGE Point TO Vertice

--- src/cluster/DistanciaEuclideaTest.java
+++ src/cluster/DistanciaEuclideaTest.java
@@ -2,20 +2,18 @@ package cluster;
 
 import static org.junit.Assert.*;
 
-import java.awt.Point;
-
 import org.junit.Test;
 
 public class DistanciaEuclideaTest {
 
 	@Test(expected = NullPointerException.class)
 	public void primerPuntoNullTest() {
-		DistanciaEuclidea.distancia(null, new Point(1, 0));
+		DistanciaEuclidea.distancia(null, new Vertice(1, 0));
 	}
 
 	@Test(expected = NullPointerException.class)
 	public void segundoPuntoNullTest() {
-		DistanciaEuclidea.distancia(new Point(1, 0), null);
+		DistanciaEuclidea.distancia(new Vertice(1, 0), null);
 	}
 
 	@Test(expected = NullPointerException.class)
@@ -26,33 +24,33 @@ public class DistanciaEuclideaTest {
 	@Test
 	// punto 1: x,y ; punto 2: x,y
 	public void distanciaEntrePuntosDistintosPositivosTest() {
-		double distancia = DistanciaEuclidea.distancia(new Point(10, 10), new Point(20, 20));
+		double distancia = DistanciaEuclidea.distancia(new Vertice(10, 10), new Vertice(20, 20));
 		assertEquals(14.142, distancia, 3);
 	}
 
 	@Test
 	// punto 1: -x,-y ; punto 2: -x,-y
 	public void distanciaEntrePuntosNegativosTest() {
-		double distancia = DistanciaEuclidea.distancia(new Point(-30, -20), new Point(-10, -50));
+		double distancia = DistanciaEuclidea.distancia(new Vertice(-30, -20), new Vertice(-10, -50));
 		assertEquals(36.056, distancia, 3);
 	}
 
 	@Test
 	// punto 1: -x,y ; punto 2: x,-y
 	public void distanciaEntrePuntosDisparejosTest() {
-		double distancia = DistanciaEuclidea.distancia(new Point(-30, 20), new Point(10, -50));
+		double distancia = DistanciaEuclidea.distancia(new Vertice(-30, 20), new Vertice(10, -50));
 		assertEquals(80.623, distancia, 3);
 	}
 
 	@Test
 	public void distanciaEntrePuntosIgualesTest() {
-		double distancia = DistanciaEuclidea.distancia(new Point(100, 100), new Point(100, 100));
+		double distancia = DistanciaEuclidea.distancia(new Vertice(100, 100), new Vertice(100, 100));
 		assertEquals(0, distancia, 0);
 	}
 
 	@Test
 	public void distanciaEntrePuntosEnCeroTest() {
-		double distancia = DistanciaEuclidea.distancia(new Point(0, 0), new Point(0, 0));
+		double distancia = DistanciaEuclidea.distancia(new Vertice(0, 0), new Vertice(0, 0));
 		assertEquals(0, distancia, 0);
 	}
 }
