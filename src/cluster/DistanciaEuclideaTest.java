e62caf33267bd3aea8e500fe7a2ead82ffe49669
Gastón <gaelceart@gmail.com>
2022-10-01 22:10:43 -0300

    FIX tests, multiply expected 112 times

--- src/cluster/DistanciaEuclideaTest.java
+++ src/cluster/DistanciaEuclideaTest.java
@@ -25,21 +25,21 @@ public class DistanciaEuclideaTest {
 	// punto 1: x,y ; punto 2: x,y
 	public void distanciaEntrePuntosDistintosPositivosTest() {
 		double distancia = DistanciaEuclidea.distancia(new Vertice(10, 10), new Vertice(20, 20));
-		assertEquals(14.142, distancia, 3);
+		assertEquals(14.142*112, distancia, 3);
 	}
 
 	@Test
 	// punto 1: -x,-y ; punto 2: -x,-y
 	public void distanciaEntrePuntosNegativosTest() {
 		double distancia = DistanciaEuclidea.distancia(new Vertice(-30, -20), new Vertice(-10, -50));
-		assertEquals(36.056, distancia, 3);
+		assertEquals(36.056*112, distancia, 3);
 	}
 
 	@Test
 	// punto 1: -x,y ; punto 2: x,-y
 	public void distanciaEntrePuntosDisparejosTest() {
 		double distancia = DistanciaEuclidea.distancia(new Vertice(-30, 20), new Vertice(10, -50));
-		assertEquals(80.623, distancia, 3);
+		assertEquals(80.623*112, distancia, 3);
 	}
 
 	@Test
