ffec54da74d72f03055bc04787b2f21f33e77897
Gastón <gaelceart@gmail.com>
2022-10-01 22:08:38 -0300

    DistanciaEuclidea.distancia() returns double similary to distance in KM

--- src/cluster/DistanciaEuclidea.java
+++ src/cluster/DistanciaEuclidea.java
@@ -1,13 +1,14 @@
 package cluster;
 
 public class DistanciaEuclidea {
-	public static long distancia(Vertice vA, Vertice vB) {
+	public static double distancia(Vertice vA, Vertice vB) {
 		if (vA == null || vB == null) {
 			throw new NullPointerException("los parametros no pueden ser null");
 		}
-		long diferenciaX = vB.get_x() - vA.get_x();
-		long diferenciaY = vB.get_y() - vA.get_y();
+		double diferenciaX = vB.get_x() - vA.get_x();
+		double diferenciaY = vB.get_y() - vA.get_y();
 		
-		return (long) Math.sqrt(Math.pow(diferenciaX, 2) + Math.pow(diferenciaY, 2));
+		return Math.sqrt(Math.pow(diferenciaX, 2) + Math.pow(diferenciaY, 2)) * 112;
+		// Multiplicado por 112 ya que 1° de diferencia en mapa es equivalente a 1/112km
 	}
 }
