51017f9668fd15c2e8a0714edd3fafefe410c115
Gastón <gaelceart@gmail.com>
2022-10-01 23:34:03 -0300

    ADD Arco.equals()

--- src/cluster/Arco.java
+++ src/cluster/Arco.java
@@ -1,5 +1,8 @@
 package cluster;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class Arco {
 	private Vertice _verticeA;
 	private Vertice _verticeB;
@@ -23,4 +26,26 @@ public class Arco {
 		return _distancia;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+
+		Arco other = (Arco) obj;
+		Set<Vertice> verticesThis = new HashSet<Vertice>();
+		Set<Vertice> verticesOther = new HashSet<Vertice>();
+		verticesThis.add(_verticeA);
+		verticesThis.add(_verticeB);
+		verticesOther.add(other._verticeA);
+		verticesOther.add(other._verticeB);
+
+		if (!verticesThis.contains(other._verticeA) || 
+				!verticesThis.contains(other._verticeB) || 
+				!verticesOther.contains(_verticeA) || 
+				!verticesOther.contains(_verticeB))
+			return false;
+		
+		return true;
+	}
 }
