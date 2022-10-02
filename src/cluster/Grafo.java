a356f1071fef0bb90eb9215f7168811c4e2c5d12
Gastón <gaelceart@gmail.com>
2022-10-01 22:12:47 -0300

    ADD method completarGrafo() ...

--- src/cluster/Grafo.java
+++ src/cluster/Grafo.java
@@ -5,9 +5,50 @@ import java.util.ArrayList;
 public class Grafo {
 	private ArrayList<Vertice> _vertices;
 	private ArrayList<Arco> _arcos;
-	
-	public Grafo () {
+
+	public Grafo() {
 		_vertices = new ArrayList<>();
 		_arcos = new ArrayList<>();
+	}
+
+	public void crearGrafo(String n) {
+		ArrayList<Vertice> coords = Filter.coordenadas(n);
+		for (Vertice v : coords)
+			agregarVertice(v);
+	}
+
+	private void agregarVertice(Vertice v) {
+		_vertices.add(v);
+	}
+	
+	public void completarGrafo() {
+		for (int i = 0; i < _vertices.size(); i++ ) {
+			for (int j = i + 1; j < _vertices.size(); j++ ) {
+				double distancia = DistanciaEuclidea.distancia(_vertices.get(i), _vertices.get(j));
+				System.out.println(distancia);
+				agregarArco(i, j, distancia);
+			}
+		}
+	}
+
+	public void agregarArco(int indexA, int indexB, double distancia) {
+		if (indexA < 0 || indexB < 0 || indexA >= _vertices.size() || indexB >= _vertices.size())
+			throw new IllegalArgumentException("Indice no válido");
+
+		_arcos.add(new Arco(_vertices.get(indexA), _vertices.get(indexB), distancia));
+		agregarVecinos(indexA, indexB);
+	}
+
+	private void agregarVecinos(int indexA, int indexB) {
+		_vertices.get(indexA).agregarVecino(indexB);
+		_vertices.get(indexB).agregarVecino(indexA);
+	}
+
+	public ArrayList<Vertice> getVertices() {
+		return _vertices;
+	}
+
+	public ArrayList<Arco> getArcos() {
+		return _arcos;
 	}
 }
