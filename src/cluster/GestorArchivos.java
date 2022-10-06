527131da4136d820dde4271d23fed3c813612e44
Leandr0Fr <leandro00fr@gmail.com>
2022-10-06 16:35:08 -0300

    Mod test distanciaEuclidea

--- src/cluster/GestorArchivos.java
+++ src/cluster/GestorArchivos.java
@@ -24,7 +24,7 @@ public class GestorArchivos {
 					coordX = Double.parseDouble(linea);
 				} else {
 					coordY = Double.parseDouble(linea);
-					marks.add(new Vertice(coordX, coordY));
+					marks.add(new Vertice(coordX, coordY, count));
 				}
 				count++;
 			}
