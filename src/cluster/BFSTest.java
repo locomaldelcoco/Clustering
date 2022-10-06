d3984d3ea89cd080cb2220e128ae3eac1fe752ba
Leandr0Fr <leandro00fr@gmail.com>
2022-10-06 16:38:27 -0300

    Mod BFSTest

--- src/cluster/BFSTest.java
+++ src/cluster/BFSTest.java
@@ -4,12 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import cluster.Assert;
-import cluster.Grafo;
-
 public class BFSTest {
 
-	@Test(expected=IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void grafoNullTest() {
 		BFS.esConexo(null);
 	}
@@ -18,63 +15,64 @@ public class BFSTest {
 	public void grafoVacioTest() {
 		assertTrue(BFS.esConexo(new Grafo()));
 	}
-	
+
 	@Test
 	public void grafoUnVerticeTest() {
 		Grafo g = new Grafo();
-		g.agregarVertice(new Vertice(0, 0));
+		g.agregarVertice(new Vertice(0, 0, 0));
 		assertTrue(BFS.esConexo(g));
 	}
-	
+
 	@Test
 	public void grafoDosVerticesAisladosTest() {
 		Grafo g = new Grafo();
-		g.agregarVertice(new Vertice(0, 0));
-		g.agregarVertice(new Vertice(1, 1));
+		g.agregarVertice(new Vertice(0, 0, 0));
+		g.agregarVertice(new Vertice(1, 1, 1));
 		assertFalse(BFS.esConexo(g));
 	}
-	
+
 	@Test
 	public void grafoDosVerticesConexoTest() {
 		Grafo g = new Grafo();
-		g.agregarVertice(new Vertice(0, 1));
-		g.agregarVertice(new Vertice(0, 2));
-		g.agregarArco(0, 1, 2);;
+		g.agregarVertice(new Vertice(0, 1, 0));
+		g.agregarVertice(new Vertice(0, 2, 1));
+		g.agregarArco(0, 1, 2);
+		;
 		assertTrue(BFS.esConexo(g));
 	}
-	
+
 	@Test
 	public void grafoInconexoTest() {
 		Grafo g = inicializarGrafoInconexo();
-		
+
 		assertFalse(BFS.esConexo(g));
 	}
-	
+
 	@Test
 	public void grafoConexoTest() {
 		Grafo g = inicializarGrafoInconexo();
-		
+
 		g.agregarArco(4, 6, 2);
 		assertTrue(BFS.esConexo(g));
 	}
-	
+
 	@Test
 	public void alcanzablesInconexoTest() {
 		Grafo g = inicializarGrafoInconexo();
-		
-		int[] esperado = {0, 1, 2, 3, 4};
+
+		int[] esperado = { 0, 1, 2, 3, 4 };
 		Assert.iguales(esperado, BFS.alcanzables(g, 0));
 	}
-	
+
 	private Grafo inicializarGrafoInconexo() {
 		Grafo g = new Grafo();
-		g.agregarVertice(new Vertice(0, 1));
-		g.agregarVertice(new Vertice(0, 2));
-		g.agregarVertice(new Vertice(0, 3));
-		g.agregarVertice(new Vertice(0, 4));
-		g.agregarVertice(new Vertice(0, 5));
-		g.agregarVertice(new Vertice(0, 6));
-		g.agregarVertice(new Vertice(0, 7));
+		g.agregarVertice(new Vertice(0, 1, 0));
+		g.agregarVertice(new Vertice(0, 2, 1));
+		g.agregarVertice(new Vertice(0, 3, 2));
+		g.agregarVertice(new Vertice(0, 4, 3));
+		g.agregarVertice(new Vertice(0, 5, 4));
+		g.agregarVertice(new Vertice(0, 6, 5));
+		g.agregarVertice(new Vertice(0, 7, 6));
 		g.agregarArco(0, 1, 1);
 		g.agregarArco(0, 2, 1);
 		g.agregarArco(1, 2, 1);
@@ -83,7 +81,6 @@ public class BFSTest {
 		g.agregarArco(3, 4, 1);
 		g.agregarArco(5, 6, 1);
 		return g;
-		
-		
+
 	}
 }
