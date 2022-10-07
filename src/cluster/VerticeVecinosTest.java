c053340e770e6044a3a2d8008b5fbc1016ee42a1
Leandr0Fr <leandro00fr@gmail.com>
2022-10-07 20:13:19 -0300

    rename VerticeTest to VerticeVecinosTEst

--- src/cluster/VerticeVecinosTest.java
+++ src/cluster/VerticeVecinosTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-public class VerticeTest {
+public class VerticeVecinosTest {
 	private Vertice vertice;
 
 	@Before
