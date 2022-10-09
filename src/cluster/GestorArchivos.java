d202a0ade0d00b2caac18c528824822efaad7dc0
Gastón <gaelceart@gmail.com>
2022-10-08 21:17:56 -0300

    REMOVE getCoordenadas();

--- src/cluster/GestorArchivos.java
+++ src/cluster/GestorArchivos.java
@@ -18,35 +18,6 @@ import com.google.gson.GsonBuilder;
 
 public class GestorArchivos {
 
-	public static ArrayList<Vertice> getCoordenadas(String file) {
-		ArrayList<Vertice> marks = new ArrayList<Vertice>();
-		int count = 0;
-		double coordX = 0.0;
-		double coordY = 0.0;
-
-		File archivoVertice = new File("files\\" + file);
-		try {
-			Scanner palabras = new Scanner(archivoVertice);
-
-			while (palabras.hasNext()) {
-				String linea = palabras.next().toString();
-
-				if (count % 2 == 0) {
-					coordX = Double.parseDouble(linea);
-				} else {
-					coordY = Double.parseDouble(linea);
-					marks.add(new Vertice(coordX, coordY));
-				}
-				count++;
-			}
-			palabras.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		return marks;
-	}
-
 	public static String[] getArchivos() {
 		return new File("files\\").list();
 	}
